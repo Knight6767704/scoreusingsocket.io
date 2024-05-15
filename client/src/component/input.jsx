ea047260f80f2ccb2e12a3c4aa8139ca7bdaa76f
Knight6767704 <knightsind6767704@gmail.com>
2024-05-15 20:21:19 +0530

    socket.io

---
+++ client/src/component/input.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Input({placeholder,handleInput,name}){
+  
+    return(
+        <div>
+        <input 
+        name={name}
+        onChange={handleInput}
+        className="input-field" placeholder={placeholder}/>
+        </div>
+    );
+}
